094b0c16290c32203ee86f8aea6fc5bc7c78b75e
Ivan <zhmur.ivan@gmail.com>
2023-12-24 14:01:03 +0200

    add new vechilces -truck

---
+++ Vechilces/Truck.cs
@@ -0,0 +1,28 @@
+﻿namespace HW_GIT_TEAM.Vechilces;
+
+public class Truck : IVechilces
+{
+    public string Brand { get; set; }
+    public string Model { get; set; }
+    public string Color { get; set; }
+    public DateTime DateOfCreating { get; set; } = DateTime.Now;
+    public void BreackDown()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Fix()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Maintenance()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Move()
+    {
+        throw new NotImplementedException();
+    }
+}
