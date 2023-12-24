c4db76b2a4bc49b451283a743d2e36632f668df1
Ivan <zhmur.ivan@gmail.com>
2023-12-24 18:48:02 +0200

    added parsel

---
+++ ParselCommon/Parsel.cs
@@ -0,0 +1,10 @@
+﻿namespace HW_GIT_TEAM.ParselCommon;
+
+public class Parsel
+{
+    public string Sender { get; set; }
+    public string Client { get; set; }
+    public double Weight { get; set; }
+    public double DeclaredPrice { get; set; }
+
+}
