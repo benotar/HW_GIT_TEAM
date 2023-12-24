4bdad8a24ece4e0434ab2106ebcd3b7dc95ed5b9
Ivan <zhmur.ivan@gmail.com>
2023-12-24 13:56:04 +0200

    add new vechilces - car

--- Vechilces/Car.cs
+++ Vechilces/Car.cs
@@ -1,6 +1,28 @@
 ﻿namespace HW_GIT_TEAM.Vechilces;
 
-public abstract class Car
+public class Car : IVechilces
 {
+    public string Brand {  get; set; }
+    public string Model { get; set; }
+    public string Color { get; set; }
+    public DateTime DateOfCreating { get; set; } = DateTime.Now;
+    public void BreackDown()
+    {
+        throw new NotImplementedException();
+    }
 
+    public void Fix()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Maintenance()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Move()
+    {
+        throw new NotImplementedException();
+    }
 }
