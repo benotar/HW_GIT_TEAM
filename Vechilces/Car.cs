58507e179c111c9762bf9936d34e1740089055bb
Ivan <zhmur.ivan@gmail.com>
2023-12-24 13:45:04 +0200

    add vechilces logics

--- Vechilces/Car.cs
+++ Vechilces/Car.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace HW_GIT_TEAM.Vechilces;
 
-namespace HW_GIT_TEAM.Vechilces
+public abstract class Car
 {
-    internal class Car
-    {
-    }
+
 }
--- Vechilces/IVechilces.cs
+++ Vechilces/IVechilces.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace HW_GIT_TEAM.Vechilces;
 
-namespace HW_GIT_TEAM.Vechilces
+public interface IVechilces
 {
-    internal interface IVechilces
-    {
-    }
+    void Move();
+    void Maintenance();
+    void BreackDown();
+    void Fix();
 }
